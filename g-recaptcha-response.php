34a4a1252ba139515399dd5bede2aa5ed91c1d91
YadamDheerajReddy <98694319+YadamDheerajReddy@users.noreply.github.com>
2024-03-18 11:07:43 +0530

    g-recaptcha-response.php

--- g-recaptcha-response.php
+++ g-recaptcha-response.php
@@ -4,17 +4,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $secretKey = "6LeiTpwpAAAAAPxwteyVSZkdE1OJoCUFpr8otbft";
     $remoteIP = $_SERVER['REMOTE_ADDR'];
 
-    // Make a POST request to the Google reCAPTCHA Server
+    if (empty($captchaResponse)) {
+        // Handle the error accordingly. For example, redirect back with an error message.
+        echo "<p>CAPTCHA not verified. Please go back and verify you are not a robot.</p>";
+        return; // Stop executing further code.
+    }
+
     $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$recaptchaResponse&remoteip=$remoteIP");
 
     $responseKeys = json_decode($response, true);
 
     if ($responseKeys["success"]) {
-        // CAPTCHA was successfully completed
-        // Your code to process the form here
     } else {
-        // CAPTCHA failed or was not completed
-        // Handle the error accordingly
     }
 }
 ?>
